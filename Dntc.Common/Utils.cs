08522583f8619eb5edc73e5a01a43a34000be7cc
KallDrexx <me@mshapiro.net>
2024-10-07 22:12:27 -0400

    Added load handlers

--- Dntc.Common/Utils.cs
+++ Dntc.Common/Utils.cs
@@ -3,4 +3,6 @@
 public static class Utils
 {
     public static string IlOffsetToLabel(int ilOffset) => $"IL_{ilOffset:x4}";
+
+    public static string LocalName(int localIndex) => $"__local{localIndex}";
 }
