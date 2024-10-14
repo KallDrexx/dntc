3f5b29bf41eb7c2e8ba7e1ed7bf2192c0919e621
KallDrexx <me@mshapiro.net>
2024-10-13 22:00:20 -0400

    Detect branch with expression stack

--- Dntc.Common/OpCodeHandling/Handlers/BranchHandlers.cs
+++ Dntc.Common/OpCodeHandling/Handlers/BranchHandlers.cs
@@ -47,6 +47,11 @@ public class BranchHandlers : IOpCodeHandlerCollection
             MethodConversionInfo currentMethod, 
             ConversionCatalog conversionCatalog)
         {
+            if (expressionStack.Count > 0)
+            {
+                throw new NotImplementedException("Branch encountered while expression in stack");
+            }
+            
             var target = (Instruction)currentInstruction.Operand;
             return new OpCodeHandlingResult(new GotoStatementSet(target.Offset));
         }
