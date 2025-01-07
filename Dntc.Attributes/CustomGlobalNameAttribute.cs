acca86a13c4474007fee200b8ead461283a54c77
KallDrexx <me@mshapiro.net>
2025-01-07 16:24:53 -0500

    Remove unused param warnings that aren't relevant

--- Dntc.Attributes/CustomGlobalNameAttribute.cs
+++ Dntc.Attributes/CustomGlobalNameAttribute.cs
@@ -4,7 +4,9 @@ namespace Dntc.Attributes;
 /// Specifies that the global it is attached to should be transpiled with a different name
 /// </summary>
 [AttributeUsage(AttributeTargets.Field)]
+#pragma warning disable CS9113 // Parameter is unread.
 public class CustomGlobalNameAttribute(string name) : Attribute
+#pragma warning restore CS9113 // Parameter is unread.
 {
     
 }
--- Dntc.Attributes/WithCAttributeAttribute.cs
+++ Dntc.Attributes/WithCAttributeAttribute.cs
@@ -5,7 +5,9 @@ namespace Dntc.Attributes;
 /// attribute position.
 /// </summary>
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Field)]
+#pragma warning disable CS9113 // Parameter is unread.
 public class WithCAttributeAttribute(string text) : Attribute
+#pragma warning restore CS9113 // Parameter is unread.
 {
     
 }
