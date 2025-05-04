3da5755f7a144a72fe86484df35885ce0a508f7e
Dan Walmsley <>
2025-05-05 00:51:50 +0100

    special handling for generic valuetypes.

--- Dntc.Common/Definitions/DotNetDefinedMethod.cs
+++ Dntc.Common/Definitions/DotNetDefinedMethod.cs
@@ -164,7 +164,7 @@ public class DotNetDefinedMethod : DefinedMethod
         return new Parameter(
             new IlTypeName(definition.ParameterType.FullName),
             definition.Name,
-            definition.ParameterType.IsByReference || definition.ParameterType.IsPointer || !definition.ParameterType.IsValueType);
+            definition.ParameterType.IsByReference || definition.ParameterType.IsPointer || !(definition.ParameterType.IsValueType || definition.ParameterType.IsGenericParameter));
     }
 
     protected override IReadOnlyList<IlTypeName> GetReferencedTypesInternal() =>
