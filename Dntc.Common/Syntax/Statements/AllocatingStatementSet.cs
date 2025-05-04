95a1f3ffeb6b7a4ed9122a182719d7454fbc428e
Dan Walmsley <>
2025-05-04 22:29:24 +0100

    remove unused method.

--- Dntc.Common/Syntax/Statements/AllocatingStatementSet.cs
+++
@@ -1,9 +0,0 @@
-namespace Dntc.Common.Syntax.Statements;
-
-public record AllocatingStatementSet(Variable Variable) : CStatementSet
-{
-    public override async Task WriteAsync(StreamWriter writer)
-    {
-        await writer.WriteLineAsync($"\t{Variable.Name} = {Variable.Type.NameInC}__Create();");
-    }
-}
